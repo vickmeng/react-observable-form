dd7b99a8022d913c5812b4a615e905776e0bbb8b
mengweikang <mengweikang@yzw.cn>
2021-06-15 17:07:09 +0800

    [feat] add docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  resolve:{
+  resolve: {
     alias: {
-      '@': path.resolve(__dirname,'./node_modules')
-    }
-
-
-  }
-})
+      "@": path.resolve(__dirname, "./node_modules"),
+    },
+  },
+  build: {
+    outDir: "docs",
+  },
+});
