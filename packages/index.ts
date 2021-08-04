4a568ec23ae7bf33b82153d0a7b3a0280fbf3a91
mengweikang <mengweikang@yzw.cn>
2021-08-04 14:30:15 +0800

    [feat] export more useful declare

--- packages/index.ts
+++ packages/index.ts
@@ -11,3 +11,12 @@ export { List } from "./items/list";
 export { FieldControl } from "./controls/fieldControl";
 export { GroupControl } from "./controls/groupControl";
 export { ListControl } from "./controls/listControl";
+
+export { FieldInternalProps, GroupInternalProps, ListInternalProps, ErrorInternalProps } from "./types/items";
+export {
+  FormControlOptions,
+  FormGroupOptions,
+  FormListOptions,
+  FormGroupControlsConfig,
+  FormListControlsConfig,
+} from "./types/control";
