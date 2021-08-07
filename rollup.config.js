9cf2ab32e1e89d83cf652fcefa0d269d0854bc39
mengweikang <mengweikang@yzw.cn>
2021-08-07 15:53:50 +0800

    [feat] adjust build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,14 @@
 import typescript from "rollup-plugin-typescript2";
-import copy from "rollup-plugin-copy";
 
 export default {
   input: "./packages/index.ts",
   output: {
-    file: "./react-observable-form-dist/index.js",
+    file: "./packages/dist/index.js",
     format: "es",
   },
   plugins: [
     typescript({
       tsconfig: "./tsconfig.build.json",
-    }),
-    copy({
-      targets: [
-        { src: "./README.md", dest: "./react-observable-form-dist" },
-        { src: "./packages/package.json", dest: "./react-observable-form-dist" },
-      ],
     }),
   ],
 };
