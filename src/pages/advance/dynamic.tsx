938aa9ae2eb5870c3bd9ee5673269ee7da8f7c21
Vickmeng <1543458592@qq.com>
2021-08-31 14:48:40 +0800

    [feat] dynamic page

--- src/pages/advance/dynamic.tsx
+++ src/pages/advance/dynamic.tsx
@@ -8,7 +8,13 @@ const DynamicPage = () => {
         动态表单
       </Typography>
 
-      <Typography paragraph>动态表单</Typography>
+      <Typography paragraph>
+        controller提供了大量的方法对对状态进行控制，我们可以通过调用这些方法实现动态表单效果。
+      </Typography>
+
+      <Typography paragraph>
+        试想一个场景，我们实现一个婚姻状况的调查问卷。如果是已婚人士，需要填写配偶的姓名，否则清空配偶姓名栏目，并禁止输入。
+      </Typography>
     </div>
   );
 };
