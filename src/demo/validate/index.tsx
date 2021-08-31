b8efc945d7f4f5653c7d442da7578e08a41c1ca6
Vickmeng <1543458592@qq.com>
2021-08-31 13:08:40 +0800

    [feat] validator page

--- src/demo/validate/index.tsx
+++ src/demo/validate/index.tsx
@@ -1,13 +1,15 @@
 import React, { useRef } from "react";
 import { TextField, FormHelperText } from "@material-ui/core";
-import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
 
-import { requiredValidator } from "../../../packages/validators";
+import { maxLengthValidator, requiredValidator } from "../../../packages/validators";
 import { Error } from "../../../packages/items/error";
 import { Field } from "../../../packages/items/field";
 import { FieldControl } from "../../../packages/controls/fieldControl";
+
 export const ValidateDemo = () => {
-  const controlRef = useRef(new FieldControl("", { validators: [requiredValidator] }));
+  const controlRef = useRef(
+    new FieldControl("这是一条过长的姓名", { dirty: true, validators: [requiredValidator, maxLengthValidator(4)] })
+  );
 
   return (
     <>
@@ -18,19 +20,32 @@ export const ValidateDemo = () => {
               <TextField
                 variant="outlined"
                 label={"姓名"}
+                // 同样可以在<Field/>中消费errors
                 error={Boolean(dirty && errors)}
                 value={value}
                 onChange={(e) => setValue(e.target.value)}
               />
-              {/*  同样可以在<Field/>中消费errors */}
-              {dirty && errors?.required && <ErrorOutlineIcon color={"error"} />}
             </>
           );
         }}
       </Field>
       <Error control={controlRef.current}>
         {({ dirty, errors }) => {
-          return <FormHelperText error>{dirty && errors?.required && "请填写姓名"}</FormHelperText>;
+          return (
+            <>
+              {dirty && (
+                <>
+                  {errors?.required && <FormHelperText error>请填写姓名</FormHelperText>}
+
+                  {errors?.maxlength && (
+                    <FormHelperText error>
+                      姓名不可大于{errors.maxlength.requiredLength}位，当前为{errors.maxlength.actualLength}位
+                    </FormHelperText>
+                  )}
+                </>
+              )}
+            </>
+          );
         }}
       </Error>
     </>
--- src/pages/basic/validate.tsx
+++ src/pages/basic/validate.tsx
@@ -18,19 +18,28 @@ const ValidatePage = () => {
         <Link to="/">自定义校验</Link>中了解更多内容。
       </Typography>
 
-      <Typography paragraph>同时,react-observable-form也为开发者准备了一些常用的校验方法。</Typography>
+      <Typography paragraph>
+        同时,react-observable-form也为开发者准备了一些常用的校验方法,<a>可在此处查看</a>。
+      </Typography>
+
+      <Typography paragraph>
+        为了让不同类型组件职能更单纯，我们提供了专门的{`<Errors/>`}
+        组件处理错误信息，也可以在其他任意的组件中消费错误，如下：
+      </Typography>
 
       <DemoCard
         demo={<ValidateDemo />}
         code={
           'import React, { useRef } from "react";\n' +
           'import { TextField, FormHelperText } from "@material-ui/core";\n' +
-          'import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";\n' +
           "\n" +
-          'import { Error,Field,FieldControl } from "react-observable-form/validators";\n' +
-          'import { required } from "react-observable-form/validators";\n' +
+          'import { maxLengthValidator, requiredValidator } from "react-observable-form/validators";\n' +
+          'import { Error,Field,FieldControl } from "react-observable-form";\n' +
+          "\n" +
           "export const ValidateDemo = () => {\n" +
-          '  const controlRef = useRef(new FieldControl("", { validators: [required] }));\n' +
+          "  const controlRef = useRef(\n" +
+          '    new FieldControl("这是一条过长的姓名", { dirty: true, validators: [requiredValidator, maxLengthValidator(4)] })\n' +
+          "  );\n" +
           "\n" +
           "  return (\n" +
           "    <>\n" +
@@ -41,19 +50,32 @@ const ValidatePage = () => {
           "              <TextField\n" +
           '                variant="outlined"\n' +
           '                label={"姓名"}\n' +
+          "                // 同样可以在<Field/>中消费errors\n" +
           "                error={Boolean(dirty && errors)}\n" +
           "                value={value}\n" +
           "                onChange={(e) => setValue(e.target.value)}\n" +
           "              />\n" +
-          "              {/*  同样可以在<Field/>中消费errors */}\n" +
-          '              {dirty && errors?.required && <ErrorOutlineIcon color={"error"} />}\n' +
           "            </>\n" +
           "          );\n" +
           "        }}\n" +
           "      </Field>\n" +
           "      <Error control={controlRef.current}>\n" +
           "        {({ dirty, errors }) => {\n" +
-          '          return <FormHelperText error>{dirty && errors?.required && "请填写姓名"}</FormHelperText>;\n' +
+          "          return (\n" +
+          "            <>\n" +
+          "              {dirty && (\n" +
+          "                <>\n" +
+          "                  {errors?.required && <FormHelperText error>请填写姓名</FormHelperText>}\n" +
+          "\n" +
+          "                  {errors?.maxlength && (\n" +
+          "                    <FormHelperText error>\n" +
+          "                      姓名不可大于{errors.maxlength.requiredLength}位，当前为{errors.maxlength.actualLength}位\n" +
+          "                    </FormHelperText>\n" +
+          "                  )}\n" +
+          "                </>\n" +
+          "              )}\n" +
+          "            </>\n" +
+          "          );\n" +
           "        }}\n" +
           "      </Error>\n" +
           "    </>\n" +
